2ac6cd0d5119e15bd2e34a97e55d901b87d031d3
e1482yama <tsubasa777@icloud.com>
2020-05-04 20:34:23 +0900

    crate Relatiomship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+
+  has_many :active_relationships,
+            class_name:  "Relationship",   
+           foreign_key: "follower_id",
+             dependent:   :destroy
+  has_many :following, 
+    through: :active_relationships,
+     source: :followed
+
+  has_many :passive_relationships,
+            class_name: "Relationship",
+            foreign_key: "followed_id",
+            dependent:  :destroy
+  has_many :followers,
+    through: :positive_relationships,
+     source:  :follower
+  
   validates :user_name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -7,4 +24,19 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+    # ユーザーをフォローする
+    def follow(other_user)
+      following << other_user
+    end
+  
+    # ユーザーをフォロー解除する
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+  
+    # 現在のユーザーがフォローしてたらtrueを返す
+    def following?(other_user)
+      following.include?(other_user)
+    end
 end
