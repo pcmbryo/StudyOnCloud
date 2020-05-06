01c997d9a819337ef30ff0cdbd11514ce09ea889
e1482yama <tsubasa777@icloud.com>
2020-05-06 16:34:45 +0900

    ユーザー作成機能完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.before_save
+      flash[:success] = "新規登録完了しました"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+    end
+end
