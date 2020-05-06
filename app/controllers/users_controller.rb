170ce5cea5548f5098ab6458aa81551327fe2064
e1482yama <tsubasa777@icloud.com>
2020-05-06 17:49:21 +0900

    ログイン・ログアウト実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.before_save
+      #ユーザー作成完了後ログインする
+      log_in @user
       flash[:success] = "新規登録完了しました"
       redirect_to @user
     else
