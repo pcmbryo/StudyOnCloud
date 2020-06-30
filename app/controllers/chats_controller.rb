d87fdf97e8e4e4de9d1898b99dae50a5bd3b8220
e1482yama <tsubasa777@icloud.com>
2020-06-30 19:39:12 +0900

    ログインしないとチャットできない＆ログアウト時にクッキー削除

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,8 @@
 class ChatsController < ApplicationController
+  include ApplicationHelper
+
+  before_action :logged_in_user, only: [:show]
+
   def show
     @messages = Chat.where(room_id: params[:id])
   end
