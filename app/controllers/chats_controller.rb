f08064f937d91fc7a449c3366760d3709f243f6f
pcmbryo <pcmbr.magic@gmail.com>
2020-06-30 19:37:46 +0900

    Merge branch 'develop' of https://github.com/pcmbryo/StudyOnCloud into develop

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,8 @@
 class ChatsController < ApplicationController
   def show
+    @page_title = "チャット"
     @messages = Chat.where(room_id: params[:id])
+    @room = Room.find(params[:id])
+    @no_header_footer = true
   end
 end
